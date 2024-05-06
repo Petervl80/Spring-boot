51f1cb669e78e58e54b06ba1f2b2e98bab236f69
pedro.vinicius <pedro.lapa1911@gmail.com>
2024-05-06 18:10:53 -0300

    Módulo 2: Spring boot Core e Dpring Framework

---
+++ src/main/java/org/example/service/ClientesService.java
@@ -0,0 +1,23 @@
+package org.example.service;
+
+import org.example.model.Cliente;
+import org.example.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientesService {
+
+    @Autowired
+    private ClientesRepository repository;
+
+    public void salvarCliente(Cliente cliente) {
+        validarCliente(cliente);
+        this.repository.persistir(cliente);
+    }
+
+    public void validarCliente(Cliente cliente) {
+        //aplica validações
+    }
+
+}
